70ec0d7aef1e1347f9b605fa88872246a5394689
Pasparstu <pasparstu@gmail.com>
2025-04-03 12:53:29 -0300

    HUD
    Implementacion del HUD con vida, magia y monedas

---
+++ Assets/HUD/hud.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var vida = 100
+var max_vida = 300
+var magia = 20
+var max_magia = 100
+var moneda = 500
+
+@onready var barra_vida = $MarginContainer/BarrasContainer/VidaContainer/Vidabar
+@onready var texto_vida = $MarginContainer/BarrasContainer/VidaContainer/VidaLabel
+@onready var barra_magia = $MarginContainer/BarrasContainer/MagiaContainer/Magiabar
+@onready var texto_magia = $MarginContainer/BarrasContainer/MagiaContainer/MagiaLabel
+@onready var texto_moneda = $MarginContainer/MondeaContainer/MonedaLabel
+
+func _ready():
+	barra_vida.max_value = max_vida
+	barra_magia.max_value = max_magia
+
+func _process(_delta):
+	barra_vida.value = vida
+	texto_vida.text = str(vida)
+	barra_magia.value = magia
+	texto_magia.text = str(magia)
+	texto_moneda.text = str(moneda)
